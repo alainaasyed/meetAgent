ad1bd32f78343e2bb9edbd36421d81e1ddbd0e8b
Alainaa <alainaasyed@gmail.com>
2026-06-13 12:55:38 +0530

    Fix port binding for Render

--- app.py
+++ app.py
@@ -183,4 +183,5 @@ def export_pdf():
                     mimetype='application/pdf')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
